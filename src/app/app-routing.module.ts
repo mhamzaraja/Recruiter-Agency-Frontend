3033e121c50ccbf13cce56e3de998526f7960122
Rajaumarali <raja.umerali61@gmail.com>
2022-01-25 16:48:16 +0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { CandidatesDetailsComponent } from './components/pages/candidates-details/candidates-details.component';
+import { CandidatesComponent } from './components/pages/candidates/candidates.component';
+import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { EmployersDetailsComponent } from './components/pages/employers-details/employers-details.component';
+import { EmployersComponent } from './components/pages/employers/employers.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { FaqComponent } from './components/pages/faq/faq.component';
+import { FavouriteJobsComponent } from './components/pages/favourite-jobs/favourite-jobs.component';
+import { HomeOneComponent } from './components/pages/home-one/home-one.component';
+import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
+import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
+import { JobDetailsComponent } from './components/pages/job-details/job-details.component';
+import { JobsComponent } from './components/pages/jobs/jobs.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { PostAJobComponent } from './components/pages/post-a-job/post-a-job.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { ResumeDetailsComponent } from './components/pages/resume-details/resume-details.component';
+import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
+import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
+
+const routes: Routes = [
+    {path: '', component: HomeOneComponent},
+    {path: 'home-two', component: HomeTwoComponent},
+    {path: 'home-three', component: HomeThreeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'employers', component: EmployersComponent},
+    {path: 'employer-details', component: EmployersDetailsComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'single-resume', component: ResumeDetailsComponent},
+    {path: 'testimonials', component: TestimonialsComponent},
+    {path: 'pricing', component: PricingComponent},
+    {path: 'faq', component: FaqComponent},
+    {path: 'coming-soon', component: ComingSoonComponent},
+    {path: 'error', component: ErrorComponent},
+    {path: 'privacy-policy', component: PrivacyPolicyComponent},
+    {path: 'terms-conditions', component: TermsConditionsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'jobs', component: JobsComponent},
+    {path: 'favourite-jobs', component: FavouriteJobsComponent},
+    {path: 'job-details', component: JobDetailsComponent},
+    {path: 'post-a-job', component: PostAJobComponent},
+    {path: 'candidates', component: CandidatesComponent},
+    {path: 'candidate-details', component: CandidatesDetailsComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'blog-details', component: BlogDetailsComponent},
+    {path: 'contact', component: ContactComponent},
+
+
+    {path: '**', component: ErrorComponent} // This line will remain down from the whole component list
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,82 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeOneComponent } from './components/pages/home-one/home-one.component';
+import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
+import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
+import { PreloaderComponent } from './components/common/preloader/preloader.component';
+import { NavbarStyleOneComponent } from './components/common/navbar-style-one/navbar-style-one.component';
+import { FooterStyleOneComponent } from './components/common/footer-style-one/footer-style-one.component';
+import { NavbarStyleTwoComponent } from './components/common/navbar-style-two/navbar-style-two.component';
+import { NavbarStyleThreeComponent } from './components/common/navbar-style-three/navbar-style-three.component';
+import { FooterStyleTwoComponent } from './components/common/footer-style-two/footer-style-two.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { EmployersComponent } from './components/pages/employers/employers.component';
+import { EmployersDetailsComponent } from './components/pages/employers-details/employers-details.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ResumeDetailsComponent } from './components/pages/resume-details/resume-details.component';
+import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { FaqComponent } from './components/pages/faq/faq.component';
+import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
+import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { JobsComponent } from './components/pages/jobs/jobs.component';
+import { FavouriteJobsComponent } from './components/pages/favourite-jobs/favourite-jobs.component';
+import { JobDetailsComponent } from './components/pages/job-details/job-details.component';
+import { PostAJobComponent } from './components/pages/post-a-job/post-a-job.component';
+import { CandidatesComponent } from './components/pages/candidates/candidates.component';
+import { CandidatesDetailsComponent } from './components/pages/candidates-details/candidates-details.component';
+import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeOneComponent,
+    HomeTwoComponent,
+    HomeThreeComponent,
+    PreloaderComponent,
+    NavbarStyleOneComponent,
+    FooterStyleOneComponent,
+    NavbarStyleTwoComponent,
+    NavbarStyleThreeComponent,
+    FooterStyleTwoComponent,
+    LoginComponent,
+    RegisterComponent,
+    EmployersComponent,
+    EmployersDetailsComponent,
+    DashboardComponent,
+    ResumeDetailsComponent,
+    TestimonialsComponent,
+    PricingComponent,
+    FaqComponent,
+    ComingSoonComponent,
+    ErrorComponent,
+    PrivacyPolicyComponent,
+    TermsConditionsComponent,
+    AboutComponent,
+    JobsComponent,
+    FavouriteJobsComponent,
+    JobDetailsComponent,
+    PostAJobComponent,
+    CandidatesComponent,
+    CandidatesDetailsComponent,
+    BlogDetailsComponent,
+    BlogComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
