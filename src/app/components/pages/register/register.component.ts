fece95ae1e74f1f998f6f11d97ced278eb3ccb46
Rajaumarali <raja.umerali61@gmail.com>
2022-01-28 18:34:23 +0500

    login and sign-up validation done

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  submitted: boolean = false;
+  form: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      phone: [null, [Validators.required,]],
+      password: ["", [Validators.required,]],
+      confirmPassword: ["", [Validators.required,]]
+      },
+      {
+        validator: this.confirmPasswordValidator("password", "confirmPassword")
+      });  
+  }
+
+  signUpForm(){
+      this.submitted = true;
+    if (this.form.valid) {
+        console.log('form submitted', this.form);
+    }
+  }
+
+  confirmPasswordValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      let control = formGroup.controls[controlName];
+      let matchingControl = formGroup.controls[matchingControlName]
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors.confirmPasswordValidator
+      ) {
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmPasswordValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
   }
 
 }
