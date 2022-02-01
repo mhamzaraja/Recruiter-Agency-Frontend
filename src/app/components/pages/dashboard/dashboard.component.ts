5f4fb1cc19a5dab1723bbf017515b26c484989a0
Rajaumarali <raja.umerali61@gmail.com>
2022-02-01 13:41:56 +0500

    basic info validation done

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean = false;
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      phone: [null, [Validators.required]],
+      summary: [null, [Validators.required,]],
+      dob: [null, [Validators.required,]],
+      gender: ["",[Validators.required]],
+      maritalStatus: [null, [Validators.required,]],
+      nationality: [null, [Validators.required,]],
+      cnic: [null, [Validators.required,]],
+      career: [""],
+      experience: [""],
+      city: [null, [Validators.required,]],
+      area: [null, [Validators.required,]],
+      expectedSalary: [null, [Validators.required,]]
+
+    })
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  baiscInfoForm(){
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    else {
+        console.log('baiscInfoForm submitted', this.form.value);
+        this.submitted = false;
+        this.form.reset();
+    }
   }
 
 }
