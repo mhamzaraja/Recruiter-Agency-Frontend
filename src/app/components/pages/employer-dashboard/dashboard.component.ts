426b805c7f4c226842a0c73a08e9862f63ecb060
Zafar abbas <abbassharaliat32@gmail.com>
2022-09-22 18:55:13 +0500

    Update Job Information

--- src/app/components/pages/employer-dashboard/dashboard.component.ts
+++ src/app/components/pages/employer-dashboard/dashboard.component.ts
@@ -3,15 +3,19 @@ import { ToastrService } from 'ngx-toastr';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { DashboardService } from './services/dashboard.service';
 import { Router } from "@angular/router";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+    styleUrls: ['./dashboard.component.scss'],
+
 })
 export class EmploerDashboardComponent implements OnInit, OnDestroy {
-
+    closeResult: string;
     public jobPostInfo = [];
+    jobPostForm: FormGroup;
     public empInformation: any;
     public companyInfo: any;
     public lastCompany: any;
@@ -25,21 +29,45 @@ export class EmploerDashboardComponent implements OnInit, OnDestroy {
     public placeholder: string = '';
     jobCounts = [];
     p: number = 1;
+    submittedJob: boolean = false;
+
 
     constructor(private formBuilder: FormBuilder,
         private toastr: ToastrService,
         private dashboardService: DashboardService,
-        private router: Router
+        private router: Router,
+        private modalService: NgbModal
     ) { }
 
     ngOnInit(): void {
         this.getAllCompanysData();
         this.getAllJobsData();
         this.getEmployeData();
+
+        this.jobPostForm = this.formBuilder.group({
+            job_title: [null, [Validators.required]],
+            job_description: [null, [Validators.required]],
+            enter_skills: [null, [Validators.required]],
+            company: [null, [Validators.required]],
+            job_location: [null, [Validators.required]],
+            required_career_level: [null, [Validators.required]],
+            salary_range: [null, [Validators.required]],
+            job_shift: ["", [Validators.required]],
+            positions_available: ["", [Validators.required]],
+            gender_requirement: ["", [Validators.required]],
+            minimum_qualification: ["", [Validators.required]],
+            years_of_experience: ["", [Validators.required]],
+            workplace_type: ["", [Validators.required]],
+            is_active: false,
+            is_sponsor: false
+        })
     }
 
     get fprj(): { [key: string]: AbstractControl } {
         return this.proejctForm.controls;
+    }
+    get fjob(): { [key: string]: AbstractControl } {
+        return this.jobPostForm.controls;
     }
 
     getAllJobsData() {
@@ -86,6 +114,56 @@ export class EmploerDashboardComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy(): void {
+
+    }
+    edit(content, i: number) {
+
+        this.modalService.open(content, { size: 'xl' });
+        this.editJob(i);
+    }
+
+    jobUpdateInfoForm() {
+        this.submittedJob = true;
+        let data = this.jobPostForm.value;
+
+        if (this.jobPostForm.invalid) {
+            this.toastr.error("this.response.message");
+        }
+        else {
+            this.dashboardService.updateJobPost(this.jobId, data).subscribe(
+                (res) => {
+                    this.toastr.success(res.message);
+                    this.getAllJobsData();
+
+                },
+                (error) => {
+                    //if (error.status == 401) this.router.navigate(['/login']);
+                    this.toastr.error(error.error.message);
+                });
+            this.submittedJob = false;
+        }
+    }
+
+    editJob(i: number) {
+        // this.saveJobBtn = false;
+        // this.updateJobBtn = true;
+        this.jobId = this.jobPostInfo[i].id;
+
+        this.jobPostForm.controls.job_title.setValue(this.jobPostInfo[i].job_title);
+        this.jobPostForm.controls.job_description.setValue(this.jobPostInfo[i].job_description);
+        this.jobPostForm.controls.enter_skills.setValue(this.jobPostInfo[i].enter_skills);
+        this.jobPostForm.controls.company.setValue(this.jobPostInfo[i].company);
+        this.jobPostForm.controls.job_location.setValue(this.jobPostInfo[i].job_location);
+        this.jobPostForm.controls.required_career_level.setValue(this.jobPostInfo[i].required_career_level);
+        this.jobPostForm.controls.salary_range.setValue(this.jobPostInfo[i].salary_range);
+        this.jobPostForm.controls.job_shift.setValue(this.jobPostInfo[i].job_shift);
+        this.jobPostForm.controls.positions_available.setValue(this.jobPostInfo[i].positions_available);
+        this.jobPostForm.controls.gender_requirement.setValue(this.jobPostInfo[i].gender_requirement);
+        this.jobPostForm.controls.minimum_qualification.setValue(this.jobPostInfo[i].minimum_qualification);
+        this.jobPostForm.controls.years_of_experience.setValue(this.jobPostInfo[i].years_of_experience);
+        this.jobPostForm.controls.workplace_type.setValue(this.jobPostInfo[i].workplace_type);
+        this.jobPostForm.controls.is_active.setValue(this.jobPostInfo[i].is_active);
+        this.jobPostForm.controls.is_sponsor.setValue(this.jobPostInfo[i].is_sponsor);
 
     }
 }
--- src/app/components/pages/post-a-job/post-a-job.component.ts
+++ src/app/components/pages/post-a-job/post-a-job.component.ts
@@ -68,13 +68,14 @@ export class PostAJobComponent implements OnInit, OnDestroy {
     }
 
     jobUpdateInfoForm() {
-        let id = this.jobId;
         this.submittedJob = true;
+        let data = this.jobPostForm.value;
+
         if (this.jobPostForm.invalid) {
             this.toastr.error("this.response.message");
         }
         else {
-            this.postAJobService.updateJobPost(this.jobId, this.jobPostForm.value).subscribe(
+            this.postAJobService.updateJobPost(this.jobId, data).subscribe(
                 (res) => {
                     this.toastr.success(res.message);
                 },
@@ -86,28 +87,28 @@ export class PostAJobComponent implements OnInit, OnDestroy {
         }
     }
 
-    // editJob(i: number) {
-    //     this.saveJobBtn = false;
-    //     this.updateJobBtn = true;
-    //     this.jobId = this.jobPostInfo[i].id;
+    editJob(i: number) {
+        // this.saveJobBtn = false;
+        // this.updateJobBtn = true;
+        this.jobId = this.jobPostInfo[i].id;
 
-    //     this.jobPostForm.controls.job_title.setValue(this.jobPostInfo[i].job_title);
-    //     this.jobPostForm.controls.job_description.setValue(this.jobPostInfo[i].job_description);
-    //     this.jobPostForm.controls.enter_skills.setValue(this.jobPostInfo[i].enter_skills);
-    //     this.jobPostForm.controls.company.setValue(this.jobPostInfo[i].company);
-    //     this.jobPostForm.controls.job_location.setValue(this.jobPostInfo[i].job_location);
-    //     this.jobPostForm.controls.required_career_level.setValue(this.jobPostInfo[i].required_career_level);
-    //     this.jobPostForm.controls.salary_range.setValue(this.jobPostInfo[i].salary_range);
-    //     this.jobPostForm.controls.job_shift.setValue(this.jobPostInfo[i].job_shift);
-    //     this.jobPostForm.controls.positions_available.setValue(this.jobPostInfo[i].positions_available);
-    //     this.jobPostForm.controls.gender_requirement.setValue(this.jobPostInfo[i].gender_requirement);
-    //     this.jobPostForm.controls.minimum_qualification.setValue(this.jobPostInfo[i].minimum_qualification);
-    //     this.jobPostForm.controls.years_of_experience.setValue(this.jobPostInfo[i].years_of_experience);
-    //     this.jobPostForm.controls.workplace_type.setValue(this.jobPostInfo[i].workplace_type);
-    //     this.jobPostForm.controls.is_active.setValue(this.jobPostInfo[i].is_active);
-    //     this.jobPostForm.controls.is_sponsor.setValue(this.jobPostInfo[i].is_sponsor);
+        this.jobPostForm.controls.job_title.setValue(this.jobPostInfo[i].job_title);
+        this.jobPostForm.controls.job_description.setValue(this.jobPostInfo[i].job_description);
+        this.jobPostForm.controls.enter_skills.setValue(this.jobPostInfo[i].enter_skills);
+        this.jobPostForm.controls.company.setValue(this.jobPostInfo[i].company);
+        this.jobPostForm.controls.job_location.setValue(this.jobPostInfo[i].job_location);
+        this.jobPostForm.controls.required_career_level.setValue(this.jobPostInfo[i].required_career_level);
+        this.jobPostForm.controls.salary_range.setValue(this.jobPostInfo[i].salary_range);
+        this.jobPostForm.controls.job_shift.setValue(this.jobPostInfo[i].job_shift);
+        this.jobPostForm.controls.positions_available.setValue(this.jobPostInfo[i].positions_available);
+        this.jobPostForm.controls.gender_requirement.setValue(this.jobPostInfo[i].gender_requirement);
+        this.jobPostForm.controls.minimum_qualification.setValue(this.jobPostInfo[i].minimum_qualification);
+        this.jobPostForm.controls.years_of_experience.setValue(this.jobPostInfo[i].years_of_experience);
+        this.jobPostForm.controls.workplace_type.setValue(this.jobPostInfo[i].workplace_type);
+        this.jobPostForm.controls.is_active.setValue(this.jobPostInfo[i].is_active);
+        this.jobPostForm.controls.is_sponsor.setValue(this.jobPostInfo[i].is_sponsor);
 
-    // }
+    }
 
     // delJob(i: number) {
     //     this.jobId = this.jobPostInfo[i].id;
